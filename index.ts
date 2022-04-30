6162e24c14e2e4db3c3d7af661434b9a3024825c
targeral <targeral@outlook.com>
2022-04-30 18:45:17 +0800

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+// get function args type
 export type Argu<F extends (input: any) => any> = F extends (
     input: infer A,
   ) => any
     ? A
     : never;
   
+// add prefix to object props
+export type AddPrefixForProp<O, Prefix> = {
+  [Property in keyof O as `${string & Prefix}${string & Property}`]: O[Property]
+};
